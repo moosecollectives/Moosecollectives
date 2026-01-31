856fa03fadce0999bc22f97800dbc0443450c4d9
-Optix - <alihammoud120@gmail.com>
2026-01-31 06:01:44 -0800

    cart badge..man i gotta sleep

--- assets/theme.js
+++ assets/theme.js
@@ -262,9 +262,8 @@ window.addEventListener('load', () => {
     if (cartCount) {
       cartCount.textContent = resolvedCart.item_count;
     }
-    const cartBadge = document.querySelector('.cart-badge');
-    if (cartBadge) {
-      cartBadge.hidden = resolvedCart.item_count === 0;
+    if (cartCount) {
+      cartCount.hidden = resolvedCart.item_count === 0;
     }
     return resolvedCart;
   };
